28806a1ab1d95656314f0f4d7aa077c18e5bdcc5
MatiDiaz <matidiazargentino21@gmail.com>
2025-06-14 00:20:38 -0300

    otro cambio del register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import FormRegister from "./FormRegister";
+import NavbarRegister from "@/components/navbarRegister/navbar";
 
 const Register: React.FC = () => {
 
     return (
         <>
+        <NavbarRegister/>
         <div className="relative w-full bg-[url('/RegisterImg.png')] bg-cover bg-center">
         aaa
         </div>
